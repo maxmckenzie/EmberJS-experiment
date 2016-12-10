38650f2328db0bcb197cc9fad540579f423f9198
Max McKenzie <hello@maxmckenzie.uk>
2016-12-10 14:24:36 +0100

    added components to interface

---
+++ app/components/expenses-.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    addExpense(){
+      console.log('thing');
+    },
+    editExpense(){
+      console.log('thing');
+    },
+    deleteExpense(){
+      console.log('thing');
+    },
+  }
+});
